1032aae5b9d7492716a71a142fc0f19a7d99977d
Besthope <874256269@qq.com>
2023-05-04 19:27:47 +0800

    Add base template and error template

--- app.py
+++ app.py
@@ -53,10 +53,18 @@ def forge():
 
 @app.route('/')
 def index():
-    user = User.query.first()
     movies = Movie.query.all()
-    return render_template('index.html', user=user, movies=movies)
+    return render_template('index.html', movies=movies)
 
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.context_processor
+def inject_user():
+    user = User.query.first()
+    return dict(user=user)  # return {'user': user}
 
 @app.cli.command()
 @click.option('--drop', is_flag=True, help='Create after drop.')
